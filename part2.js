09f6d50591c988a7624f927ca1a9b8a6ab5e7359
Esther <126341347+athulyaesther777@users.noreply.github.com>
2024-08-07 22:12:26 +0530

    Add files via upload

---
+++ part2.js
@@ -0,0 +1,7 @@
+let firstName = "John";
+let lastName = "Doe";
+
+// Concatenate the first and last names with a space in between
+let fullName = firstName + " " + lastName;
+
+console.log(fullName); // Output: John Doe
