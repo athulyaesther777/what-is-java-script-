6bdf67cbd321abd0cc724b1456963f86ae6452af
Esther <126341347+athulyaesther777@users.noreply.github.com>
2024-08-07 22:08:40 +0530

    Add files via upload

---
+++ part1.js
@@ -0,0 +1,5 @@
+/ This line declares a variable named 'message' and assigns it the string value "Hello, World!"
+let message = "Hello, World!";
+
+// This line logs the value of the 'message' variable to the console
+console.log(message);
