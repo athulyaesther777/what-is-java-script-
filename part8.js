e4295cd8d24ed84c80bd65ddf21a77f38c8af1e4
Esther <126341347+athulyaesther777@users.noreply.github.com>
2024-08-08 22:23:53 +0530

    Create part8.js

---
+++ part8.js
@@ -0,0 +1,10 @@
+export class Person {
+    constructor(name, age) {
+        this.name = name;
+        this.age = age;
+    }
+
+    greet() {
+        console.log(`Hello, my name is ${this.name}.`);
+    }
+}
